050e2671ef1e38be1478c7a6679d45249fd0f6c5
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-23 19:54:23 -0700

    add api calls WIP

--- js/species.js
+++ js/species.js
@@ -1,11 +1,35 @@
 // Business Logic for species API call
 
+export default class Species {
+  static allSpecies(number) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://swapi.dev/api/species/`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
-
-
-
-
-
-
-
-
+  static getSpecies(number) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://swapi.dev/api/species/${number}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
