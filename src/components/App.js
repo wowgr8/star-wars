7220ecea4efba3d9e4778f5883585f77de046046
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 00:35:44 -0700

    add background image

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Control from './Control';
+import bgImage from '../assets/img/star-background.jpeg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="hero min-h-screen" style={{ backgroundImage:`url(${bgImage})`}}>
       <Header />
       <Control />
       <Footer />
