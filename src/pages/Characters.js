af44c732aea1c5da478d33039fd8354796182400
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 15:21:38 -0700

    add axios import statement. add async function that gets people data obj and add it to useEffect. Add peopleData useState hooks for api response. Add loop in return() displaying character names.

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Characters() {
+  const [ peopleData, setPeopleData ] = useState([]);
+  const [ characterList, setCharacterList ] = useState([]);
+  
+  useEffect(()=> {
+    getPeopleData()
+  }, [])
+
+  const getPeopleData = async () =>{
+    const data = await axios.get("https://swapi.dev/api/people/");
+    setPeopleData(data.data);
+  };
+
+  console.log("data: ", peopleData)
+  console.log("data: ", peopleData.results)
+  console.log("data: ", peopleData.results.map((character)=>{return character.name}))
+
   return (
-    
-    <div className='container py-40 mx-auto border-4 border-solid border-yellow-300'>
-      Characters
+    <div className=''>
+      <div className='text-zinc-100'>
+        Characters
+      </div>
+      <div >
+        {peopleData.results.map((character)=>{ 
+          return(
+            <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
+              <li>
+                <p>{character.name}</p>
+              </li>
+            </ul>
+          )
+        })}
+      </div>
     </div>
   )
 }
