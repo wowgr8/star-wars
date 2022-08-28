6a019323efdf6f114bca7ebb8ded5e16b8bf3e42
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-27 23:33:36 -0700

    add logo import statement and class styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/img/Star_Wars_Logo.png';
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav className="border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-gray-900">
+      <div className="logo">
+        <img src={logo} />
+      </div>
+    </nav>
   )
 }
 
