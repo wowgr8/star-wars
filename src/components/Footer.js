40dbd2499b1281a72aa19c17dcd18fd3ef8e5813
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-27 23:22:37 -0700

    add typo fix to all className attributes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,18 @@ import { AiOutlineMail } from 'react-icons/ai';
 
 function Footer() {
   return (
-    <footer class="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800">
-      <span class="text-sm sm:text-center text-gray-400">&copy; 2022 <a href="https://github.com/wowgr8" class="hover:underline">/wowgr8</a>.
+    <footer className="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800">
+      <span className="text-sm sm:text-center text-gray-400">&copy; 2022 <a href="https://github.com/wowgr8" className="hover:underline">/wowgr8</a>.
       </span>
-      <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0">
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0">
         <li>
-          <a href='https://github.com/wowgr8' class="mr-4 hover:underline md:mr-6"><BsGithub /></a>
+          <a href='https://github.com/wowgr8' className="mr-4 hover:underline md:mr-6"><BsGithub /></a>
         </li>
         <li>
-          <a href='https://www.linkedin.com/in/cesar-aug-lopez/' class="mr-4 hover:underline md:mr-6"><BsLinkedin /></a>
+          <a href='https://www.linkedin.com/in/cesar-aug-lopez/' className="mr-4 hover:underline md:mr-6"><BsLinkedin /></a>
         </li>
         <li>
-          <a href='mailto:lopez.cesar.aug@gmail.com' class="mr-4 hover:underline md:mr-6"><AiOutlineMail /></a>
+          <a href='mailto:lopez.cesar.aug@gmail.com' className="mr-4 hover:underline md:mr-6"><AiOutlineMail /></a>
         </li>
       </ul>
     </footer>
