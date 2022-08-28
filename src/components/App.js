463cc06d18da5d8f02582cf71ddf535b0682b3a8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-27 23:38:34 -0700

    add header and control import statments

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Footer from './Footer';
+import Header from './Header';
+import Control from './Control';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Control />
       <Footer />
     </div>
   );
