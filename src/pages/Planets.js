8b1b594e09aabfa078a44910837d24e432ea0c0f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 21:46:30 -0700

    add useEffect/state/context and axios import statements, add useContext hooks, add getPlanetData() function.

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+import { Context } from '../utils/Context';
+import Species from './Species';
 
 function Planets() {
+  const [ planetData, setPlanetData ] = useState();
+  // const [ characterList, setCharacterList ] = useState([]);
+
+  const { selectedPlanet, setCurrentView } = useContext(Context);
+  
+  useEffect(()=> {
+    getPlanetData();
+  }, [])
+
+  const getPlanetData = async () =>{
+    const data = await axios.get(`${selectedPlanet}`);
+    setPlanetData(data.data);
+  };
+  // console.log("data: ", planetData)
+
+  // // Gets called once peopledata is fetched. Sets characterList - used to return name in loop.
+  // useEffect(()=> {
+  //   if(planetData){
+  //     console.log("planet data acquired!")
+  //     console.log(planetData, planetData.name)
+  //     console.log(planetData.name)
+
+  //     // getCharacters()
+  //   }
+  // }, [planetData])
+
+  // const getCharacters = () =>{
+  //   setCharacterList(planetData.results);
+  //   // console.log(characterList)
+  // }
+
+  // function with onclick handler. Gets character homeworld. Navigates to planet.js with that state. which is used in the getPlanet api call...
+  // const handleOnCharacterSelect = (value) => {
+  //   console.log(value);
+  //   // loop through list of characters. Find match to the selected value by name. if match setplanet to character.homeworld.
+  //   {characterList.map((character)=>{ 
+  //     if (character.name === value){
+  //       console.log(character.homeworld, " Line 60())()")
+  //       // setSelectedPlanet(character.homeworld);
+  //       setCurrentView(<Planets />)
+  //     }
+  //   })}
+  // }
+
+  // useEffect(()=> {
+  //   if(selectedPlanet){
+  //     console.log("PLANET SELECTED")
+  //     console.log(selectedPlanet)
+
+  //   }
+  // }, [selectedPlanet])
+
   return (
-    <div>Planets</div>
+    <div className='container mx-auto text-gray-50 pl-40'>
+      <p>HomeWorld Details!</p>
+
+    </div>
   )
 }
 
