5c5a2729645aa4a76713c87ed8f29d9406cb4a28
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-29 02:31:14 -0700

    add back button

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { Context } from '../utils/Context';
 import Species from './Species';
+import Characters from './Characters';
 
 function Planets() {
   const [ planetData, setPlanetData ] = useState();
   const [ viewSpecies, setViewSpecies ] = useState(false);
 
-  const { selectedPlanet } = useContext(Context);
+  const { selectedPlanet, setCurrentView } = useContext(Context);
   
   useEffect(()=> {
     getPlanetData();
@@ -26,8 +27,12 @@ function Planets() {
     setViewSpecies(prev => !prev)
   }
 
+  const handleBackButton = () => {
+    setCurrentView(<Characters />)
+  }
+
   return (
-    <div className='grid place-items-center text-gray-50'>
+    <div className='grid place-items-center text-gray-50 -mt-6'>
       <p>HomeWorld Details!</p>
         {planetData ? <>
           <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
@@ -56,6 +61,9 @@ function Planets() {
                 </tr>
               </tbody>
             </table>
+            <button onClick={(handleBackButton)} className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Back to Character Select
+            </button>
           </div>  
           {viewSpecies ? <Species /> : null } </>
         : 
