f56cd20705de3151967f15ae66cf8b8f7b4cb754
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 22:18:29 -0700

    add destructured props, getSpeciesData(), and useEffect() for getSpeciesData().

--- src/pages/Species.js
+++ src/pages/Species.js
@@ -2,8 +2,20 @@ import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { Context } from '../utils/Context';
 
-function Species() {
-  
+function Species(props) {
+  const { planetData } = props;
+  const [ speciesData, setSpeciesData ] = useState();
+
+  useEffect(()=> {
+    getSpeciesData();
+  }, [])
+
+  const getSpeciesData = async () =>{
+    const data = await axios.get("https://swapi.dev/api/species/");
+    setSpeciesData(data.data);
+  };
+  console.log("data: ", speciesData)
+
   return (
     <div class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <div className='text-red-500'>Species</div>
