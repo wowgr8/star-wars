21a9fce4bc6cf7712841b7371d18e0776a1f9ac2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 18:46:05 -0700

    add react-router-dom import statement and created routes for each page.

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,29 @@ import Footer from './Footer';
 import Header from './Header';
 import Control from './Control';
 import bgImage from '../assets/img/star-background.jpeg';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route 
+  } from "react-router-dom";
+import Characters from '../pages/Characters';
+import Planets from '../pages/Planets';
+import Species from '../pages/Species';
 
 function App() {
   return (
-    <div className="hero min-h-screen" style={{ backgroundImage:`url(${bgImage})`}}>
-      <Header />
-      <Control />
-      <Footer />
-    </div>
+    <Router>
+      <div className="hero min-h-screen" style={{ backgroundImage:`url(${bgImage})`}}>
+        <Header />
+        <Control />
+        <Routes>
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/planets" element={<Planets />} />
+          <Route path="/species" element={<Species />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
