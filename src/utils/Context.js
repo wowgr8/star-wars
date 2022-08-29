fd37043db323eb8e628ffbce39002b5b48548e4a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-29 00:08:52 -0700

    add Characters.js import statement. Add currentView useState hook and set Character component tag as default.

--- src/utils/Context.js
+++ src/utils/Context.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import Characters from "../pages/Characters";
   
 export const Context = React.createContext();
 
 export const ContextProvider = ({ children }) => {
   const [ selectedPlanet, setSelectedPlanet ] = useState();
-  
+  const [ currentView, setCurrentView ] = useState(()=> <Characters />);
+
   return (
-    <Context.Provider value={{ selectedPlanet, setSelectedPlanet }}>
+    <Context.Provider value={{ selectedPlanet, setSelectedPlanet, currentView, setCurrentView }}>
       {children}
     </Context.Provider>
   );
