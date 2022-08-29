3458e5393106b2d9e1bd63a3f7ed7a828dfbb576
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 21:48:06 -0700

    add card with table showing planet details. Add ternary operator to conditionally render table.

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -59,7 +59,33 @@ function Planets() {
   return (
     <div className='container mx-auto text-gray-50 pl-40'>
       <p>HomeWorld Details!</p>
-
+        {planetData ? 
+          <div class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{planetData.name}</h5>
+            <table className="table-auto">
+              <tbody className="font-normal text-gray-700 dark:text-gray-400">
+                <tr>
+                  <td>Population: </td>
+                  <td>{planetData.population}</td>
+                </tr>
+                <tr>
+                  <td>Residents: </td>
+                  <td>{planetData.residents.length}</td>
+                </tr>
+                <tr>
+                  <td>Terrain: </td>
+                  <td>{planetData.terrain}</td>
+                </tr>
+                <tr>
+                  <td>Climate: </td>
+                  <td>{planetData.climate}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        : 
+          "....Loading" 
+        }
     </div>
   )
 }
