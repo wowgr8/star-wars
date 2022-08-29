16776b601b51fd111d81dbf5b35658f6afe1adfe
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 22:13:10 -0700

    add card container and Species title placeholder. Add useEffect/state/context and axios import statements.

--- src/pages/Species.js
+++ src/pages/Species.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+import { Context } from '../utils/Context';
 
 function Species() {
+  
   return (
-    <div>Species</div>
+    <div class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div className='text-red-500'>Species</div>
+    </div>
   )
 }
 
