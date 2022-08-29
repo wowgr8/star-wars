895c60a3243ffddd36060fce0be35bbcdc09fe51
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-29 00:14:25 -0700

    add route for Control and rearranged div.

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,29 @@ import Footer from './Footer';
 import Header from './Header';
 import Control from './Control';
 import bgImage from '../assets/img/star-background.jpeg';
+import Characters from '../pages/Characters';
+import Planets from '../pages/Planets';
+import Species from '../pages/Species';
 import { 
   BrowserRouter as Router,
   Routes,
   Route 
   } from "react-router-dom";
-import Characters from '../pages/Characters';
-import Planets from '../pages/Planets';
-import Species from '../pages/Species';
 
 function App() {
   return (
-    <Router>
-      <div className="hero min-h-screen" style={{ backgroundImage:`url(${bgImage})`}}>
+    <div className="hero min-h-screen" style={{ backgroundImage:`url(${bgImage})`}}>
+      <Router>
         <Header />
-        <Control />
         <Routes>
+          <Route path="/" element={<Control />} />
           <Route path="/characters" element={<Characters />} />
           <Route path="/planets" element={<Planets />} />
           <Route path="/species" element={<Species />} />
         </Routes>
         <Footer />
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
