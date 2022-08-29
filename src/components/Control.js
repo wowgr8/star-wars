0c5b6d568140e1eea7c7dfb9aacf0255c9809d39
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-29 00:10:40 -0700

    add useContext and currentView Context hook and replace Characters component tag with it.

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext }from 'react';
+import { Context } from '../utils/Context';
 
 function Control() {
+  const { currentView } = useContext(Context);
+
   return (
-    <div>Control</div>
+    <div className="container py-40 mx-auto border-4 border-solid border-yellow-300">
+      <div >
+        {currentView}
+      </div>
+    </div>
   )
 }
 
