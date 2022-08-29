bac874d72a2b4939ad9caaa0b6eae3807011ab4c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-28 18:52:02 -0700

    add smaller image size for logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import logo from '../assets/img/Star_Wars_Logo.png';
 
 function Header() {
   return (
-    <nav className="border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-gray-900">
+    <nav className="border-gray-200 sm:px-4 rounded bg-gray-900 mb-32">
       <div className="logo">
-        <img src={logo} />
+        <img className="h-20" src={logo} />
       </div>
     </nav>
   )
