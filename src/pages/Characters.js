c642ea70bc7ee39758dd764e1aa22afe05325762
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-09-02 12:12:46 -0700

    add getNextPeopleData() and button to render 10 more characters.

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -7,7 +7,7 @@ function Characters() {
   const [ peopleData, setPeopleData ] = useState();
   const [ characterList, setCharacterList ] = useState([]);
 
-  const { setSelectedPlanet, setCurrentView } = useContext(Context);
+  const { setSelectedPlanet, setCurrentView, allCharactersList, setAllCharactersList } = useContext(Context);
   
   useEffect(()=> {
     getPeopleData()
@@ -19,9 +19,14 @@ function Characters() {
     setPeopleData(data.data);
   };
 
+  const getNextPeopleData = async () =>{
+    const data = await axios.get(peopleData.next);
+    setCharacterList(characterList => characterList.concat(data.data.results))
+  };
+
   useEffect(()=> {
     if(peopleData){
-      getCharacters()
+      getCharacters();
     }
   }, [peopleData])
 
@@ -42,6 +47,11 @@ function Characters() {
   return (
     <div className='grid place-items-center text-gray-50'>
       <p>CHARACTERS!</p>
+      <div onClick={()=>getNextPeopleData()}>
+        <button>
+        Click here to Load More Characters
+        </button>
+      </div>
       <div className="flex justify-center items-center relative inline-flex">
         <svg className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fillRule="nonzero"/></svg>
         <select onChange={(e)=> handleOnCharacterSelect(e.target.value)} className=" flex justify-center items-center border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
